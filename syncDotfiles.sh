15f6538a9c8c8e4ac8d66c3ada5eb21228e2f091
Isotop7 <hendrik@hr94.de>
2021-11-25 07:47:59 +0100

    initial commit

---
+++ syncDotfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### home folder
+
+cp ~/.Xdefaults .Xdefaults
+cp ~/.vimrc .vimrc
+cp ~/.zshrc .zshrc
+
+### .config folder
+cp -r ~/.config/colorschemes .config/
+cp -r ~/.config/gtk-2.0 .config/
+cp -r ~/.config/gtk-3.0 .config/
+cp -r ~/.config/openbox .config/
+cp -r ~/.config/polybar .config/
